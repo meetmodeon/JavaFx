31b07ed546831fee5c84ce89ed1a14f62f122b0b
Meet_Yadav <120273933+meetmodeon@users.noreply.github.com>
2024-02-26 19:37:31 +0545

    This is to design menu Bar

---
+++ javamenubar/HelloController.java
@@ -0,0 +1,11 @@
+package com.example.javamenubar;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+
+    public void newMethod(){
+        System.out.println("You selected the New menu item!");
+    }
+}
