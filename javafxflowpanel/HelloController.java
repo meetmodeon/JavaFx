ab2de09d517197f163b011f7879983a490ee3942
Meet_Yadav <120273933+meetmodeon@users.noreply.github.com>
2024-02-27 18:45:23 +0545

    Add files via upload

---
+++ javafxflowpanel/HelloController.java
@@ -0,0 +1,11 @@
+package com.example.javafxflowpanel;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+
+public class HelloController {
+@FXML
+    private FlowPane myFlowPane;
+
+}
